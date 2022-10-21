2a6acf2c07a56569e64c176dbed7103f04943209
ahmadrifanf <60764438+ahmadrifanf@users.noreply.github.com>
2022-10-21 20:10:23 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -9,14 +9,14 @@ st.set_page_config(layout="wide")
 st.image("banner.jpg")
 
 st.title("Bangkitkan e-Commerce Indonesia")
-st.caption("oleh: Ahmad RIfan Ferdiyansyah")
+st.caption("oleh: Ahmad Rifan Ferdiyansyah")
 
 c1a, c1b = st.columns([2,1], gap="large")
 with c1a:
-    st.markdown("Pandemi COVID-19 telah mempengaruhi sebagian kegiatan bisnis mulai dari operasional, pemasaran, penjualan, hingga cara berkomunikasi dengan pelanggan. Situasi ini terbukti mampu mendorong e-commerce nasional. Menurut _Indonesia E-Commerce Assosiation_ (IdEA), penjualan e-commerce mengalami peningkatan sebesar 25% selama pandemi dan volume transaksi meningkat hingga 78% dibanding tahun 2019. Pertumbuhan e-commerce mendadak meningkat berkali-kali lipat dibandingkan yang diprediksi.")
-    st.markdown("Meskipun demikian, ternyata hasil Survei Statistik e-Commerce tahun 2021 yang diselenggarakan oleh Badan Pusat Statistik (BPS) menunjukkan bahwa **sekitar 72,39% pelaku usaha e-commerce mengeluhkan pendapatan mereka yang menurun** pada tahun 2021. Mayoritas pelaku usaha e-commerce mengatakan bahwa penurunan pendapatan mereka berada pada rentang 50-75% jika dibandingkan tahun 2020. Kondisi ini masih sama dengan hasil survei tahun sebelumnya, dimana mayoritas pelaku usaha e-commerce juga mengeluhkan penurunan pendapatan akibat pandemi.")
-    st.caption("Mengapa ini bisa terjadi?")
-    st.markdown("Untuk menjawabnya mari kita lihat terlebih dahulu bagaimana profil dari e-commerce di Indonesia.")
+    st.markdown("Pandemi COVID-19 telah mempengaruhi sebagian kegiatan bisnis mulai dari operasional, pemasaran, penjualan, hingga cara berkomunikasi dengan pelanggan. Situasi ini terbukti mampu mendorong _e-commerce_ nasional. Menurut _Indonesia E-Commerce Assosiation_ (IdEA), penjualan _e-commerce_ mengalami peningkatan sebesar 25% selama pandemi dan volume transaksi meningkat hingga 78% dibanding tahun 2019. Pertumbuhan _e-commerce_ mendadak meningkat berkali-kali lipat dibandingkan yang diprediksi.")
+    st.markdown("Meskipun demikian, ternyata hasil Survei Statistik _e-commerce_ tahun 2021 yang diselenggarakan oleh Badan Pusat Statistik (BPS) menunjukkan bahwa **sekitar 72,39% pelaku usaha _e-commerce_ mengeluhkan pendapatan mereka yang menurun** pada tahun 2021. Mayoritas pelaku usaha _e-commerce_ mengatakan bahwa penurunan pendapatan mereka berada pada rentang 50-75% jika dibandingkan tahun 2020. Kondisi ini masih sama dengan hasil survei tahun sebelumnya, dimana mayoritas pelaku usaha _e-commerce_ juga mengeluhkan penurunan pendapatan akibat pandemi.")
+    st.caption("Mengapa ini bisa terjadi? Bagaimana cara mengatasinya?")
+    st.markdown("Untuk menjawabnya mari kita lihat terlebih dahulu profil dari usaha _e-commerce_ di Indonesia.")
     
 with c1b:
     label1 =['Meningkat','Sama','Menurun']
@@ -34,26 +34,27 @@ with c1b:
             colors = color,
             wedgeprops = {"linewidth":1, "edgecolor":"white"})
     ax1.set_title("Proporsi e-Commerce Menurut Dampak Pandemi\nTerhadap Pendapatan Usaha, 2021")
-    ax1.set_xlabel("sumber: BPS (diolah)")
+    ax1.set_xlabel("sumber: BPS, 2021 (diolah)")
     st.pyplot(fig1)
 
 st.markdown('''---''')
 st.header("Profil e-Commerce Indonesia")    
 
-st.markdown("_Electronic Commerce_ (e-Commerce) berdasarkan _Organization for Economic Co-Operation and Development_ (OECD, 2009) adalah penjualan atau pembelian barang atau jasa, yang dilakukan melalui jaringan komputer dengan metode yang secara spesifik dirancang untuk tujuan menerima atau melakukan pesanan. Barang atau jasa dipesan dengan metode tersebut, tetapi pembayaran dan pengiriman utama barang atau jasa tidak harus dilakukan secara online. Transaksi e-Commerce mencakup pemesanan melalui halaman website, ekstranet maupun _Electronic Data Interchange_ (EDI), e-mail, media sosial (Facebook, Instagram, dan lainnya), serta _instant messaging_ (WhatsApp, Line, dan lainnya) dan tidak mencakup pemesanan yang dibuat melalui telepon (baik _fixed-line_ maupun _mobile phone_) dan faksimili.")
+st.markdown("_Electronic Commerce_ (_e-commerce_) berdasarkan _Organization for Economic Co-Operation and Development_ (OECD, 2009) adalah penjualan atau pembelian barang atau jasa, yang dilakukan melalui jaringan komputer dengan metode yang secara spesifik dirancang untuk tujuan menerima atau melakukan pesanan. Barang atau jasa dipesan dengan metode tersebut, tetapi pembayaran dan pengiriman utama barang atau jasa tidak harus dilakukan secara online. Transaksi _e-commerce_ mencakup pemesanan melalui halaman website, ekstranet maupun _Electronic Data Interchange_ (EDI), e-mail, media sosial (Facebook, Instagram, dan lainnya), serta _instant messaging_ (WhatsApp, Line, dan lainnya) dan tidak mencakup pemesanan yang dibuat melalui telepon (baik _fixed-line_ maupun _mobile phone_) dan faksimili.")
 st.markdown("Berangkat dari definisi tersebut, BPS kemudian memperkirakan jumlah usaha _e-commerce_ di Indonesia yaitu mencapai 2.361.423 usaha. Adapun sebaran usaha _e-commerce_ banyak terdapat di Pulau Jawa, yaitu sekitar 75,15% dari total usaha _e-commerce_. Fenomena ini tentunya berkaitan dengan lokasi yang dekat dengan pusat perekonomian dan ketersediaan fasilitas pendukung usaha seperti akses internet yang memadai.")
 
 st.subheader("Proporsi Usaha e-Commerce Indonesia")
 
 c2a, c2b = st.columns([2,1], gap="large")
 with c2a:
-    value2 = {'media' : ['website','website','email','email','instant messaging','instant messaging','media sosial','media sosial','marketplace','marketplace'],
-           'menggunakan': ['ya','tidak','ya','tidak','ya','tidak','ya','tidak','ya','tidak',],
-           'persentase' : [2.38,97.62,10.42,89.58,93.98,6.02,54.66,45.34,21.64,78.36]}
-    value2 = pd.DataFrame(value2)
-
-    fig2 = px.bar(value2, x="media", y="persentase", color="menggunakan", title="Berdasarkan Media Penjualan")
-    st.plotly_chart(fig2)
+    #ya = [2.38,10.42,93.98,54.45,21.64]
+    #tidak = [97.62,89.58,6.02,45.34,78.36]
+    #value2 = pd.DataFrame(data={'ya':ya, 'tidak':tidak})
+    #value2.index = ['website','email','instant messaging','media sosial','marketplace']
+    #st.bar_chart(value2)
+    
+    st.caption("Berdasarkan Media Penjualan")
+    st.image("img.png")
 
 with c2b:
     filter3 = st.selectbox('Berdasarkan',
@@ -91,25 +92,26 @@ with c2b:
     fig3, ax3 = plt.subplots()
     ax3.pie(value3,
             explode = explode3,
-            labels = label3,
+            #labels = label3,
             autopct = '%1.1f%%',
             #shadow = TRUE,
             startangle = 90,
             colors = color3,
             wedgeprops = {"linewidth":1, "edgecolor":"white"})
     ax3.set_title(filter3)
+    ax3.legend(label3)
     st.pyplot(fig3)
     
 st.caption("sumber: BPS, 2021 (diolah)")
 st.markdown("")
 
-st.markdown("Dari grafik tersebut dapat kita simpukan bahwa sebagian besar pelaku usaha merupakan usaha e-commerce non-formal, dengan ciri-ciri:")
+st.markdown("Dari grafik tersebut dapat kita simpukan bahwa sebagian besar pelaku usaha merupakan usaha _e-commerce_ non-formal, dengan ciri-ciri:")
 st.markdown("1. Mayoritas menggunakan _instant messaging_ dan media sosial sebagai media penjualan")
 st.markdown("2. Nilai pendapatan total sebagian besar _e-commerce_ masih di bawah 300 juta rupiah")
 st.markdown("3. Metode pembayaran yang paling sering digunakan yaitu _Cash on Delivery_ (COD) atau pembayaran secara tunai")
 st.markdown("4. Mayoritas pemilik/penanggung jawab _e-commerce_ berusia 35-44 tahun")
 st.markdown("5. 75,36 persen pemilik/penanggung jawab _e-commerce_ berpendidikan SMA/SMK sederajat ke bawah")
-st.markdown("Jika kita merujuk pada Undang- Undang Nomor 20 Tahun 2008 tentang Usaha Mikro, Kecil dan Menengah (UMKM) maka dapat dikatakan bahwa e-commerce Indonesia masih didominasi oleh usaha mikro, yaitu usaha dengan pendapatan kurang dari 300 juta setahun. Selain itu, kendala utama dari kegiatan mereka sebagian besar disebabkan oleh kurangnya permintaan atas barang dan jasa selama pandemi (48,74%). Sehingga, untuk mengatasi keluhan penurunan pendapatan dari para pelaku usaha _e-commerce_ harus diterakan kebijakan terkait peningkatan strategi pemasaran e-commerce yang sangat mendukung keberlangsungan usaha mikro.")
+st.markdown("Jika kita merujuk pada Undang- Undang Nomor 20 Tahun 2008 tentang Usaha Mikro, Kecil dan Menengah (UMKM) maka dapat dikatakan bahwa _e-commerce_ Indonesia masih didominasi oleh usaha mikro. Dan kita tahu bahwa usaha mikro sangat rentan terhadap guncangan ekonomi makro seperti yang telah terjadi pada awal pandemi. Oleh sebab itu, untuk mengatasi keluhan penurunan pendapatan pelaku usaha _e-commerce_ harus diterapkan kebijakan-kebijakan yang sangat mendukung keberlangsungan usaha mikro. Umumnya, kendala utama dari kegiatan mereka disebabkan oleh karena kurangnya permintaan atas barang dan jasa selama pandemi (48,74%).")
 st.markdown("")
 
 c3a, c3b = st.columns(2, gap="large")
@@ -145,6 +147,7 @@ with c3a:
 
     ax4.set_title('Keikutsertaan Pelatihan')
     ax5.set_title('Jenis Pelatihan')
+    ax4.set_xlabel("sumber: BPS, 2021 (diolah)")
 
     theta1, theta2 = ax4.patches[0].theta1, ax4.patches[0].theta2
     center, r = ax4.patches[0].center, ax4.patches[0].r
@@ -172,15 +175,14 @@ with c3b:
 
 st.markdown('''---''')
 st.subheader("Kesimpulan")
-st.markdown("Peningkatan transaksi _e-commerce_ yang terjadi selama pandemi ternyata belum mampu menyentuh seluruh pelaku usaha _e-commerce_. Hal ini tampak dari banyaknya keluhan atas penurunan pendapatan dari para pelaku usaha, khususnya pada skala usaha mikro (pendapatan kurang dari 300 juta setahun). Selain itu, pendidikan pelaku usaha yang tidak terlalu tinggi dan rendahnya keikutsertaan dalam pelatihan menyebabkan mereka kesulitan dalam terus menarik minat konsumen untuk membeli produk mereka. Oleh sebab itu, diperlukan kebijakan perihal _e-commerce_ yang sangat mendukung keberlangsungan usaha mikro.")
+st.markdown("Peningkatan transaksi _e-commerce_ yang terjadi selama pandemi ternyata belum mampu menyentuh seluruh pelaku usaha _e-commerce_. Hal ini tampak dari banyaknya keluhan atas penurunan pendapatan dari para pelaku usaha, khususnya pada skala usaha mikro (pendapatan kurang dari 300 juta setahun). Selain itu, pendidikan pelaku usaha yang tidak terlalu tinggi dan rendahnya keikutsertaan dalam pelatihan menyebabkan sebagian besar usaha _e-commerce_ kesulitan untuk menarik minat konsumen agar membeli produk mereka. Oleh sebab itu, diperlukan strategi-strategi pengembangan usaha _e-commerce_ yang dapat diterapkan pada usaha dengan skala mikro.")
 
 st.markdown('''---''')
 st.subheader("Saran")
-st.markdown("Pengadaan pelatihan bagi pelaku usaha _e-commerce_ skala mikro dapat menjadi salah satu upaya untuk meningkatkan kemampuan pelaku usaha _e-commerce_, khususnya pemberian materi perihal promosi yang efektif dalam memasarkan produk mereka. Selain itu, pendampingan para ahli dalam proses pengembangan usaha juga mungkin diperlukan agar mereka dapat terus bertahan dalam menghadapi era digitalisasi ekonomi yang masih terus berkembang hingga saat ini.")
+st.markdown("Pengadaan pelatihan bagi pelaku usaha _e-commerce_ skala mikro dapat menjadi salah satu upaya untuk meningkatkan kemampuan pelaku usaha _e-commerce_. Pelatihan perihal tata cara promosi dengan optimaliasi pemanfaatan teknologi informasi diharapkan mampu meningkatkan pemasarkan produk mereka. Selain itu, pendampingan para ahli juga mungkin diperlukan dalam upaya pengembangan usaha agar mereka memiliki kemampuan untuk bersaing dalam menghadapi era digitalisasi ekonomi yang terus berkembang hingga saat ini.")
     
 st.markdown('''---''')
 st.subheader("Referensi")
-st.markdown("_BPS. 2021. Statistik e-Commerce Tahun 2021. https://www.bps.go.id/publication/2021/12/17/667821e67421afd2c81c574b/statistik-e-commerce-2021.html. Jakarta._")
-st.markdown("_iPrice. 2022. Find Out E-commerce Competition in Indonesia. https://iprice.co.id/insights/mapofecommerce/en/. diakses 12 Oktober 2022 pukul 01.24 WIB._")
-st.markdown("_OECD. 2022. Electronic Commerce. https://stats.oecd.org/glossary/detail.asp?ID=4721. diakses 12 Oktober 2022 pukul 01.25 WIB._")
-st.markdown("_Redseer. 2022. Consumer Internet in Indonesia. https://redseer.com/reports/consumer-internet-in-indonesia/. diakses 18 Oktober 2022 pukul 00.04 WIB._")
+st.text("BPS. 2021. Statistik e-Commerce Tahun 2021. Jakarta.")
+st.text("OECD. 2022. Electronic Commerce. https://stats.oecd.org/glossary/detail.asp?ID=4721. diakses 12 Oktober 2022 pukul 01.25 WIB")
+st.text("Redseer. 2022. Consumer Internet in Indonesia. https://redseer.com/reports/consumer-internet-in-indonesia/. diakses 18 Oktober 2022 pukul 00.04 WIB")
